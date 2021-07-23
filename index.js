ac68a8c05c746f5a581718bdc7d12404d2d21e5c
Daniel Poles <daniel@dpulm.online>
2021-07-23 17:07:17 +0200

    clustering

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const async = require("async");
 const cors = require("cors");
 const Database = require("./database");
 const { getInfo, convert, convertThumb } = require("./util");
+const { performance, PerformanceObserver } = require("perf_hooks");
+
+const obs = new PerformanceObserver((list, observer) => {
+  console.log(list.getEntries()[0]);
+  performance.clearMarks();
+  // observer.disconnect();
+});
+obs.observe({ entryTypes: ["measure"], buffered: true });
 
 var multer = require("multer");
 var storage = multer.memoryStorage();
@@ -31,6 +39,7 @@ app.post("/upload", upload.array("img", 5), async (req, res) => {
     };
   });
 
+  // performance.mark('startConvert');
   // create a queue object with concurrency 2
   var q = async.queue(function (i, callback) {
     async.parallel(
@@ -70,6 +79,8 @@ app.post("/upload", upload.array("img", 5), async (req, res) => {
   await q.push(img);
 
   await drain;
+  // performance.mark('stopConvert');
+  // performance.measure('convert', 'startConvert', 'stopConvert')
   res.sendStatus(204);
 });
 
@@ -86,6 +97,24 @@ app.get("/list", async (req, res) => {
       _.pick(object.toObject(), ["_id", "width", "fileType", "fileName"])
     )
   );
+});
+
+app.get("/list/ids", async (req, res) => {
+  const cursor = Database.Pic.aggregate([
+    {
+      $sample: { size: Number(req.query.num) || 1024},
+    },
+    {
+      $project: {
+        _id: 1,
+      },
+    },
+  ]).cursor();
+  for await (const d of cursor) {
+    res.write(d._id + "\n");
+  }
+  // res.chunkedEncoding = true;
+  res.end();
 });
 
 app.get("/list/:name", async (req, res) => {
@@ -108,9 +137,28 @@ app.get("/img/:id", async (req, res) => {
         ["type", doc.fileType],
         ["data", doc.data],
       ])
-      .then(() => client.expire(req.params.id, client.config.ttl));
+      // .then(() => client.expire(req.params.id, config.get('redis.ttl')));
     res.type(doc.fileType);
     res.end(doc.data);
+  }
+});
+
+app.get("/img/size/:id", async (req, res) => {
+  if ((await client.exists(req.params.id)) === 1) {
+    const data = await client.hget(req.params.id, "data");
+    return res.status(200).send(String(data.length));
+  } else {
+    const doc = await Database.Pic.findById(req.params.id, {
+      "info.mimeType": 1,
+      data: 1,
+    });
+    client
+      .hmset(req.params.id, [
+        ["type", doc.fileType],
+        ["data", doc.data],
+      ])
+      .then(() => client.expire(req.params.id, client.config.ttl));
+    res.status(200).send(String(doc.data.length));
   }
 });
 
